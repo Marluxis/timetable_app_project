0a67dd1f151a64a0bcadb5e2d37287f6be20a7f7
Dimitar Velkov <64478960+Marluxis@users.noreply.github.com>
2025-08-02 00:04:00 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+   
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -21,10 +21,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        
         applicationId = "com.example.timetable_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+       
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -33,8 +32,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -48,3 +46,5 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation(kotlin("stdlib-jdk7"))
 }
+
+// TODO: for Release add unique ID etc.
